5cd91176e8bff0e823611e62917e54f30a729e97
Roshan Sharma <rs127@fordham.edu>
2026-06-27 16:10:33 -0400

    chore: bump to 0.1.7 to surface updated README on npm

--- bin/factory.js
+++ bin/factory.js
@@ -18,7 +18,7 @@ program
     '  Powered by SuperPlane (orchestration) + Render (deployment).\n' +
     '  Every issue becomes: spec → code → tests → deployed preview → PR.\n'
   )
-  .version('0.1.6');
+  .version('0.1.7');
 
 program
   .command('init')
